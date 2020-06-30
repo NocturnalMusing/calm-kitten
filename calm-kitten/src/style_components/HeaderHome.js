c117402961bb4fe51aa032b271739fcd387ffeb2
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-06-30 16:09:06 -0400

    Test styling

--- calm-kitten/src/style_components/HeaderHome.js
+++ calm-kitten/src/style_components/HeaderHome.js
@@ -5,10 +5,10 @@ export default class Header extends Component {
     render() {
         return (
             <div className='header'>
-                <p>Instructions:</p> 
-                <p><em>type</em> "I need a laugh" <em>for a joke</em></p>
-                <p><em>type</em> "Tell me something kind" <em>to read an uplifting quote</em></p>
-                <p><em>type</em> "I need help" <em>if you feel (idk smth here)</em></p>
+                <h3><strong>Instructions</strong>:</h3> 
+                <p>type <em>"I need a laugh"</em> for a joke</p>
+                <p>type <em>"Tell me something kind"</em> to read an uplifting quote</p>
+                <p>type <em>"I need help"</em> for professional aid</p>
             </div>
         )
     }
