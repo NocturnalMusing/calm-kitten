1e309c01627bcf03ce619223cb30eb9dbd5baa0b
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-06-30 09:32:13 -0400

    Day two begin

--- calm-kitten/src/page_components/Quote.js
+++ calm-kitten/src/page_components/Quote.js
@@ -6,21 +6,35 @@ import '../App.css'
 
 export default function Quote() {
     let [quote, updateQuote] = useState([])
+    let [ random, updateRandom ] = useState(0)
+
+    let randomInt = () => {
+        let randomNum = Math.floor(Math.random() * 1643)
+
+        updateRandom(randomNum)
+        console.log(random)
+    }
 
     const apiCall = async () => {
         const data = await axios.get('https://type.fit/api/quotes')
+        
+        // console.log(data.data[random].author)
 
-        console.log(data.data)
     }
 
     useEffect(() => {
         apiCall()
+
+        updateQuote()
+
+        randomInt()
     }, [])
 
     return (
         <>
             <div>
                 <p>This will be a quote</p>
+                <p>And this will be an author</p>
             </div>
         </>
     )
