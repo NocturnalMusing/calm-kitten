aa399c5ff2823757be8f326ce9b973dbfbbfc26d
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-07-01 13:55:29 -0400

    Style Help page

--- calm-kitten/src/page_components/Help.js
+++ calm-kitten/src/page_components/Help.js
@@ -9,17 +9,18 @@ export default class Help extends Component {
         return (
             <>
                 <Header />
-                <div>
+                <div className='reach-out'>
                     <p>Asking for help is always the first step, and I promise you aren't alone!</p>
                     <p>Please reach out, if not to someone you know, then to a stranger.</p>
                     <br />
                     <p>National Suicide Prevention Lifeline</p>
-                    <p>1-800-273-8255</p>
+                    <a href='tel:1-800-273-8255' className='nspl-number'>1-800-273-8255</a>
+                    <p className='smalltext'>click to call</p>
                     <br />
-                    <p>The world needs you in it! <i class="fas fa-heart"></i></p>
-                </div>
-                <div>
-                    <img src={SadBlink} />
+                    <p>The world needs you in it!</p>
+                    <i class="fas fa-heart"></i>
+                
+                    <img src={SadBlink} className='sad-blink'/>
                 </div>
             </>
         )
